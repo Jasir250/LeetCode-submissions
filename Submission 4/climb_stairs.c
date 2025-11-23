66f416ee81892066aec304d7304722c0904effeb
Jasir <k250701@nu.edu.pk>
2025-11-23 23:49:37 +0500

    Create climb_stairs.c

---
+++ Submission 4/climb_stairs.c
@@ -0,0 +1,12 @@
+int climbStairs(int n) {
+
+    if(n < 0)
+    {
+        return 1;
+    }else if(n <= 1){
+        return 1;
+    }else{
+        return climbStairs(n - 1) + climbStairs(n - 2) ;
+    }
+
+}
